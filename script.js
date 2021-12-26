fd350edddd0875da12d7b331c4c1c17fd41970d0
Julien-Lebourgeois <julien.lebourge@gmail.com>
2021-12-26 01:05:09 +0100

    V2 animation to make appears the colored pictures and descriptions

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(function(){
 
+    var $mainMenuItems = $("#main-menu").children("li"),
+    totalMainMenuItems = $mainMenuItems.length,
+    openedIndex = -1,
+
+    init = function(){
+        $mainMenuItems.children(".images").click(function(){
+            var newIndex = $(this).parent().index(),
+            $item = $mainMenuItems.eq(newIndex),
+            $colorImage = $item.find(".color");
+
+            $colorImage.animate({left:"0px"}, 250);
+            $item.animate({width:"420px"},250);
+
+            openedIndex = newIndex;
+        });
+    };
+
+    init();
     
 
 });
